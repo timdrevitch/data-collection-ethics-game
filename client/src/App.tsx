6baf8eec3c510f201f959db281900bbd818e0e51
timdrevitch <110933291+timdrevitch@users.noreply.github.com>
2022-11-01 20:25:24 -0400

    🚗 Added car image to homescreen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
+import { FC, useState } from "react";
 import "./App.css";
 
-const App = () => {
+const App: FC = (): JSX.Element => {
+  const [url, setUrl] = useState<string>(
+    "https://data-ethics-game.herokuapp.com/api/v1"
+  ); // https://data-ethics-game.herokuapp.com/api/v1 || http://localhost:4000/api/v1
+
   return (
-    <h1 style={{ width: "100%", textAlign: "center", marginTop: "5rem" }}>
-      Hello World
-    </h1>
+    <div style={{ position: "relative" }}>
+      <img
+        style={{ width: "100%" }}
+        src={require("./Assets/car.jpeg")}
+        alt="carimage"
+      />
+      <h1
+        style={{
+          position: "absolute",
+          width: "100%",
+          textAlign: "center",
+          top: "5rem",
+        }}
+      >
+        Data Collection Ethics Game
+      </h1>
+    </div>
   );
 };
 
