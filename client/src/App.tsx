5535d030a98c9c765175da14844e49f5ed00a8dd
timdrevitch <110933291+timdrevitch@users.noreply.github.com>
2022-11-01 21:29:10 -0400

    🏠 Title screen updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,17 +9,18 @@ const App: FC = (): JSX.Element => {
   return (
     <div style={{ position: "relative" }}>
       <img
-        style={{ width: "100%" }}
+        style={{ width: "100%", opacity: "30%" }}
         src={require("./Assets/car.jpeg")}
         alt="carimage"
       />
       <h1
         style={{
           position: "absolute",
-          width: "40%",
-          top: "2em",
+          width: "100%",
+          top: "1em",
           marginLeft: "5%",
-          fontSize: "2.5vw",
+          fontSize: "3.5vw",
+          textShadow: "1px 1px 8px #fff, 1px 1px 8px #ccc",
         }}
       >
         Data Collection Ethics Game
@@ -28,7 +29,7 @@ const App: FC = (): JSX.Element => {
         style={{
           position: "absolute",
           width: "40%",
-          top: "7em",
+          top: "7.5em",
           marginLeft: "5%",
           fontSize: "1.5vw",
           color: "orange",
@@ -38,14 +39,60 @@ const App: FC = (): JSX.Element => {
       </div>
       <button
         style={{
+          cursor: "pointer",
           position: "absolute",
-          width: "10%",
-          bottom: "20em",
-          right: "5%",
-          // marginRight: "5%",
+          width: "30%",
+          height: "5%",
+          top: "30em",
+          right: "-5%",
+          fontSize: "1vw",
+          backgroundColor: "orange",
+          border: "1px solid white",
+          borderRadius: "15px",
+          textAlign: "left",
+          paddingLeft: "2em",
+          color: "white",
+        }}
+      >
+        Continue
+      </button>
+      <button
+        style={{
+          cursor: "pointer",
+          position: "absolute",
+          width: "30%",
+          height: "5%",
+          top: "35em",
+          right: "-5%",
+          fontSize: "1vw",
+          backgroundColor: "orange",
+          border: "1px solid white",
+          borderRadius: "15px",
+          textAlign: "left",
+          paddingLeft: "2em",
+          color: "white",
         }}
       >
         New Game
+      </button>
+      <button
+        style={{
+          cursor: "pointer",
+          position: "absolute",
+          width: "30%",
+          height: "5%",
+          top: "40em",
+          right: "-5%",
+          fontSize: "1vw",
+          backgroundColor: "orange",
+          border: "1px solid white",
+          borderRadius: "15px",
+          textAlign: "left",
+          paddingLeft: "2em",
+          color: "white",
+        }}
+      >
+        Settings
       </button>
     </div>
   );
