25496bdd4cc99dac5706ab9ca6501e1b90fab307
timdrevitch <110933291+timdrevitch@users.noreply.github.com>
2022-11-01 20:59:18 -0400

    ✍️ Updating the title screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,13 +16,37 @@ const App: FC = (): JSX.Element => {
       <h1
         style={{
           position: "absolute",
-          width: "100%",
-          textAlign: "center",
-          top: "5rem",
+          width: "40%",
+          top: "2em",
+          marginLeft: "5%",
+          fontSize: "2.5vw",
         }}
       >
         Data Collection Ethics Game
       </h1>
+      <div
+        style={{
+          position: "absolute",
+          width: "40%",
+          top: "7em",
+          marginLeft: "5%",
+          fontSize: "1.5vw",
+          color: "orange",
+        }}
+      >
+        Tim Drevitch & Fangtai Bao
+      </div>
+      <button
+        style={{
+          position: "absolute",
+          width: "10%",
+          bottom: "20em",
+          right: "5%",
+          // marginRight: "5%",
+        }}
+      >
+        New Game
+      </button>
     </div>
   );
 };
