61ba07fa23bdeaa88ab7224bfd423c809a23cacc
timdrevitch <110933291+timdrevitch@users.noreply.github.com>
2022-11-15 19:01:37 -0500

    request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,9 @@ import PlayerStats from "./Pages/PlayerStats";
 import Game from "./Pages/Game";
 
 const App: FC = (): JSX.Element => {
-  const [url, setUrl] = useState<string>("http://localhost:4000/api/v1");
+  const [url, setUrl] = useState<string>(
+    "https://data-ethics-game.herokuapp.com/api/v1"
+  );
   // https://data-ethics-game.herokuapp.com/api/v1 || http://localhost:4000/api/v1
   const [userEmail, setUserEmail] = useState<string | null>(null);
   const [userFirstName, setUserFirstName] = useState<string | null>(null);
